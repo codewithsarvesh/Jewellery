f3931ed46c60d98a634089f6877c74a3781287e1
codewithsarvesh <143922159+codewithsarvesh@users.noreply.github.com>
2023-09-03 10:29:13 +0530

    Add files via upload

---
+++ Hypothenious.java
@@ -0,0 +1,18 @@
+import java.io.*;
+class Angle
+{
+	public static void main(String args[])throws IOException
+	{
+		BufferedReader br=new BufferedReader(Inputstreamreader(System.in));
+		System.out.println("Enter Hypothenious Length:");
+		int h;
+		h=Integer.parseInt(br.readLine);
+		double a,b;
+		a=h/2;
+		x=math.sqrt(3);
+		b=x*h/2;
+		System.out.println("front of 30:"+a);
+		System.out.println("front of 60:"+b);
+	}
+}
+		
